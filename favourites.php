31886a015bb6c7a0addfa90c3001fef9d0c38a52
= <izabeladrabek@Izabelas-MacBook-Pro.local>
2020-04-28 00:09:06 +0200

    Ff

---
+++ favourites.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    <body>
+        <h3> The Positions </h3>
+        <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                    <td>14</td>
+                    
+                </tr>
+                
+                
+                <h2> A Description of the players </h2>
+                <dl>Forwards</dl>
+                <dd> - Expected to Scrim, normally players are strongest on the pitch and enjoy contact.</dd>
+                <dl>Backs</dl>
+                <dd>Players who enjoy running at fast paces and have good long passing ability.</dd>
+        
+               
+        
+        <?php
+        // put your code here
+        ?>
+                
+                 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="MainStyles.css" />
+    </head>
+ 
+    
+   
+    <body>
+           <header>My name is Izzy</header>
+        
+            <a href="https://www.instagram.com/iz_zydrabek/"> click here </a>
+            <img  src="image/Rugby.jpg" width = 300 height = 400 alt ='pic of me' />
+               
+            <ul>
+                <H3>What I love about rugby</H3>
+                <li> The contact
+                <li> Pace of the game
+                <li> Teammates
+                   
+            </ul>
+       
+            
+            <p id="greenit"> I love CSS learning</p>    
+            
+            <p class="ugly"> I love oranges </p>
+                
+        <?php
+        // put your code here
+        ?>
+                
+ 
+    </body>
+    
+   
+</html>
